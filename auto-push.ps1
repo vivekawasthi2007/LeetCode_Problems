4604c492f017406fc6fcbb995ea351f011471b05
Vivek Awasthi <vivekawasthi2007@gmail.com>
2026-08-30 23:23:54 +0530

    Auto update

---
+++ auto-push.ps1
@@ -0,0 +1,30 @@
+$folder = Get-Location
+
+$watcher = New-Object System.IO.FileSystemWatcher
+$watcher.Path = $folder
+$watcher.IncludeSubdirectories = $true
+$watcher.EnableRaisingEvents = $true
+
+$action = {
+    Start-Sleep -Seconds 3
+
+    git add .
+
+    $changes = git status --porcelain
+
+    if ($changes) {
+        git commit -m "Auto update"
+        git push
+        Write-Host "Changes pushed to GitHub."
+    }
+}
+
+Register-ObjectEvent $watcher Created -Action $action
+Register-ObjectEvent $watcher Changed -Action $action
+
+Write-Host "Auto GitHub Push is running..."
+Write-Host "Press Ctrl+C to stop."
+
+while ($true) {
+    Start-Sleep -Seconds 5
+}
