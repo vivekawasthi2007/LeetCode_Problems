518eee6bea06a12c5043e35ec19c84fe867b3968
Vivek Awasthi <vivekawasthi2007@gmail.com>
2026-09-06 14:43:53 +0530

    initial update

---
+++ LeetCode_Problem2.java
@@ -0,0 +1,72 @@
+ class Main {
+
+    // Linked List Node
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+            int digit = sum % 10;
+
+            current.next = new ListNode(digit);
+            current = current.next;
+
+            if (l1 != null)
+                l1 = l1.next;
+
+            if (l2 != null)
+                l2 = l2.next;
+        }
+
+        return dummy.next;
+    }
+
+    // Print Linked List
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+
+            if (head.next != null)
+                System.out.print(" -> ");
+
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // 2 -> 4 -> 3 = 342
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        // 5 -> 6 -> 4 = 465
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode result = addTwoNumbers(l1, l2);
+
+        System.out.print("Result: ");
+        printList(result);
+    }
+}
